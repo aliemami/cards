82a72f8224371429176334c1274a65ab356fec22
ali emami <afromac@alis-MacBook-Pro.local>
2014-01-29 00:32:56 -0500

    cards files

---
+++ Card.java
@@ -0,0 +1,53 @@
+
+
+public class Card {
+	//number cards in each suit
+	private static final int NUMBEROFCARDS_IN_EACH_DECK = 13;
+
+	//value of the card 
+	private Value value = null;
+
+	//suit of the card
+	private Suit suit = null;
+
+	public enum Suit { 
+		DIAMONDS("\u2666"),HEARTS("\u2665"),CLUBS("\u2663"),SPADES("\u2660"); 
+		public String value;
+		//the value parameter, is the unicode for different symbols (heart, diamonds, clubs and Spades)
+		private Suit (String value){
+			this.value = value;
+		}
+	}
+
+
+	public enum Value {	
+		TWO("2",2),THREE("3",3),FOUR("4",4),FIVE("5",5),SIX("6",6),SEVEN("7",7),EIGHT("8",8),NINE("9",9),TEN("10",10),JACK("J",11),QUEEN("Q",12),KING("K",13),ACE("A",14);
+		//value if the card
+		public int value;
+		//name of the card 
+		public String name;
+		private Value (String name, int value){
+			this.value = value;
+			this.name = name;
+		}
+	}
+
+	/*
+	 *Constructor for the card class
+	 *@param i  a number between 0 and 52 represent each card in a deck
+	 */
+	public Card(int i) {
+		if (i<0 || i > 51) {
+			throw new RuntimeException( i + " is not representation of a card, the number has to be between 0 and 51.");
+		}
+		int s = i / NUMBEROFCARDS_IN_EACH_DECK;
+		int c = i % NUMBEROFCARDS_IN_EACH_DECK;
+		value = Value.values()[c];
+		suit = Suit.values()[s];
+	}
+
+	//print the card
+	public String toString () {
+		return (this.value.name + "" + this.suit.value);
+	}
+}
---
+++ CardsDeck.java
@@ -0,0 +1,74 @@
+
+
+import java.util.Collections;
+import java.util.EmptyStackException;
+import java.util.Random;
+import java.util.Stack;
+
+public class CardsDeck{
+	//numver if cards in a deck
+	private static final int NUMCARDS = 52;
+
+	private Stack<Card> deck = null;
+
+	//the whole class could be define as a singleton depends on the application 
+	//constructor creates and initializes the deck of cards
+	public CardsDeck() {
+		deck = new Stack<Card>();
+		for (int i=0; i< NUMCARDS;i++) {
+			deck.push(new Card(i));
+		}
+	}
+
+	//pop the top card and return null if there is no element in the stack or other exceptions
+	public Card deal() {
+		try {
+			return deck.pop();
+		}catch (EmptyStackException e) {
+		}catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return null;
+	}
+
+	//shuffle the existing deck
+	public void shuffle(){
+		long seed = System.nanoTime();
+		Collections.shuffle(deck, new Random(seed));
+	}
+
+
+	public String toString() {
+		//stringBuffer "theoretically" is faster but for small string manipulation
+		//string class is just fast and simple 
+		String str = "";
+		for (int i=0; i < deck.size() ; i++) {
+			str += deck.elementAt(i) + " \n";
+		}
+		return str + " size:" + deck.size();
+	}
+
+
+	//just for representation having a unit test is the right way to go
+	public static void main(String [] args) {
+		//create a new deck of card
+		CardsDeck cardsDeck = new CardsDeck();
+		//print it
+		System.out.println(cardsDeck);
+
+		//shuffle it and print it again
+		cardsDeck.shuffle();
+		System.out.println(cardsDeck);
+		System.out.println("\n--------------");
+
+		//deal a card and print it to the screen 
+		Card card = cardsDeck.deal();
+		System.out.println("dealt card:" + card);
+		System.out.println("\n--------------");
+
+		//reprint the deck
+		System.out.println("print it again");
+		System.out.println(cardsDeck);
+
+	}
+}
