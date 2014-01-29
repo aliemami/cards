8c93cef760fce314e9ca65ea514c5a8afe57ae19
ali emami <afromac@alis-MacBook-Pro.local>
2014-01-29 00:49:31 -0500

    changing the location of Ace from end of the suit after king to be the first card

--- Card.java
+++ Card.java
@@ -21,7 +21,7 @@ public class Card {
 
 
 	public enum Value {	
-		TWO("2",2),THREE("3",3),FOUR("4",4),FIVE("5",5),SIX("6",6),SEVEN("7",7),EIGHT("8",8),NINE("9",9),TEN("10",10),JACK("J",11),QUEEN("Q",12),KING("K",13),ACE("A",14);
+		ACE("A",1),TWO("2",2),THREE("3",3),FOUR("4",4),FIVE("5",5),SIX("6",6),SEVEN("7",7),EIGHT("8",8),NINE("9",9),TEN("10",10),JACK("J",11),QUEEN("Q",12),KING("K",13);
 		//value of the card
 		public int value;
 		//name of the card 
